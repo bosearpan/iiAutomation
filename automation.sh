cbec99332d5f745cbb0d791591f1a3ea38694be6
bosearpan <bosearpan93@gmail.com>
2021-06-02 01:31:29 +0530

    Task 2 & Task 3

--- automation.sh
+++ automation.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
-#Perform an update of the package details and the package list at the start of the script.
-sudo apt update -y
-#Install the apache2 package if it is not already installed.
+#===========================Apache Installation=================================================================================
+sudo apt update -y &> /dev/null
 dpkg -s apache2 &> /dev/null
-while [ $? -ne 0 ]
-do
-sudo apt-get install apache2
-done
-sudo service apache2 status | grep "running"  #Ensure that the apache2 service is running.
 if [ $? -ne 0 ]
 then
-sudo systemctl enable apache2                  #Ensure that the apache2 service is enabled
+echo "Installing apache"
+sudo apt-get install apache2 &> /dev/null
+fi
+sudo service apache2 status | grep "running" &> /dev/null 	 #Ensure that the apache2 service is running.
+if [ $? -ne 0 ]
+then
+sudo systemctl enable apache2                 				 #Ensure that the apache2 service is enabled
 sudo systemctl start apache2.service
 fi
-#Create a tar archive of apache2 access logs and error logs that are present in the /var/log/apache2/ directory and place the tar into the /tmp/ directory
+#===========================Backup apache log ==================================================================================
 s3_bucket="upgrad-arpan"
 myname="Arpan"
 timestamp=$(date '+%d%m%Y-%H%M%S')
 cd /var/log/apache2/
-tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar *.log
-
-#The script should run the AWS CLI command and copy the archive to the s3 bucket.
+tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar *.log &> /dev/null
+size=$(sudo du -sh /tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+#============================ Bookeep ===========================================================================================
+if [ -e /var/www/html/inventory.html ]
+then
+echo "<br>httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; ${size}" >> /var/www/html/inventory.html
+else
+echo "<b>Log Type &nbsp;&nbsp;&nbsp;&nbsp; Date Created &nbsp;&nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp;&nbsp;&nbsp; Size</b><br>" > /var/www/html/inventory.html
+echo "<br>httpd-logs &nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp; ${size}" >> /var/www/html/inventory.html
+fi
+#============================Pushing to AWS S3 bucket============================================================================
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
-s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar &> /dev/null
+#=============================Cron Job to run the script everyday================================================================
+if [ ! -e /etc/cron.d/automation ]
+then
+sudo touch /etc/cron.d/automation
+sudo echo "0 0 * * *	root	/root/Automation_Project/automation.sh --crond" | sudo tee -a /etc/cron.d/automation > /dev/null
+fi
+#++++++++++++++++++++++++++++++++++End of Play+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++i
